f924efe3adbc9053079fd6b588b2cddb891a7f90
Ignacio Martinez <ignaciomartinez@dhcp64-131-121-7.kentlaw.edu>
2017-03-31 11:26:39 -0500

    Selected boxes and added color to box 1 and box 3

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("hello");
+var boxes = document.querySelectorAll(".box");
+
+boxes[0].style.backgroundColor = "blue";
+
+boxes[2].style.backgroundColor = "yellow";
+
+boxes[0].style.backgroundColor = "blue";
